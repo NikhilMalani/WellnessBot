9f3fd29941458f3b3988ad2cbaed71e05948e052
Nikhil K. Malani <NikhilMalani@users.noreply.github.com>
2017-07-12 10:14:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ restService.post('/health', function(req, res) {
     var add = ((+q1 + +q2 + +q3 + +q4 + +q5 + +q6 + +q7 + +q8 + +q9 + +q10)/55)*100;
 	var finalScore = add.toFixed(2);
     var speech = finalScore ? "Thanks " + firstName + "! Your wellness score out of 100 is " + finalScore + ". Please check your inbox for some custom feedback - be sure to monitor your spam folder and mark as 'Not Spam'." : "Sorry, some error occurred.";
-    var api_key = 'key-2017d84d2750326f0a79dc52b95bff98fd612017';
-    var domain = 'sandbox2017163f50fc3f2840f5a6e316f198b386e52017.mailgun.org';
+    var api_key = 'key-dfed82d5fc390d4092d066c02542c4a5';
+    var domain = 'sandbox1a6a6686cad74e5880d94f7bf969406d.mailgun.org';
     var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
     var data = {
     from: 'Excited User <me@samples.mailgun.org>',
-    to: 'info@therapy.coach',
+    to: 'nkmalani@gmail.com',
     subject: 'Response from '+ firstName + " "+ lastName,
     html : "<html>These are the responses. <br><br> FirstName: '"+firstName+"'<br><br>LastName: '"+lastName+"'<br><br>Email: '"+emailAddress+"'<br><br>I am physically healthy: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q1+"'<br><br>My diet/nutrient intake is balanced: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q2+"'<br><br>I take good care of myself: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q3+"'<br><br>Overall, I feel safe: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q4+"'<br><br>My work/home environments suit my needs: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q5+"'<br><br>I can love and feel loved: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q6+"'<br><br>I feel stimulated and engaged in life: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q7+"'<br><br>My daily activities are meaningful: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q8+"'<br><br>I take time to meditate and connect with nature: (1) Strongly Disagree (2) Disagree (3) Neutral (4) Agree (5) Strongly Agree: '"+q9+"'<br><br>Rate your health overall out of 10: '"+q10+"' </html>"
     };
